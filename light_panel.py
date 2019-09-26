3c23691a94d47fc3e2d131215085a756c6ed8af5
kalisp <petr.kalis@gmail.com>
2019-09-26 20:07:54 +0200

    Light Panel - added Scroll area
    Fixed remove items

--- light_panel.py
+++ light_panel.py
@@ -34,10 +34,25 @@ class LightItem(QtWidgets.QWidget):
         self.create_connections()
 
     def create_widgets(self):
-
+        self.light_type_btn = QtWidgets.QPushButton()
+        self.light_type_btn.setFixedSize(20, 20)
+        
+        self.light_visibility_btn = QtWidgets.QCheckBox()
+        
+        self.light_name_label = QtWidgets.QLabel("Placeholder")
+        self.light_name_label.setFixedWidth(120)
+        self.light_name_label.setAlignment(QtCore.Qt.AlignCenter)
+        
 
     def create_layout(self):
-
+        main_layout = QtWidgets.QHBoxLayout(self)
+        main_layout.setContentsMargins(0, 0, 0, 0)
+        main_layout.addWidget(self.light_type_btn)
+        main_layout.addWidget(self.light_visibility_btn)
+        main_layout.addWidget(self.light_name_label)
+        
+        main_layout.addStretch()
+        
 
     def create_connections(self):
         pass
@@ -57,6 +72,8 @@ class LightPanel(QtWidgets.QDialog):
             self.setWindowFlags(QtCore.Qt.Tool)
 
         self.resize(500, 260)
+        
+        self.light_items = []
 
         self.create_widgets()
         self.create_layout()
@@ -66,6 +83,7 @@ class LightPanel(QtWidgets.QDialog):
         self.refreshButton = QtWidgets.QPushButton("Refresh Lights")
 
     def create_layout(self):
+        # header
         header_layout = QtWidgets.QHBoxLayout()
         header_layout.addSpacing(100)
         header_layout.addWidget(QtWidgets.QLabel("Light"))
@@ -82,22 +100,60 @@ class LightPanel(QtWidgets.QDialog):
         button_layout = QtWidgets.QHBoxLayout()
         button_layout.addStretch()
         button_layout.addWidget(self.refreshButton)
+        
+        light_list_wdg = QtWidgets.QWidget()
+                
+        self.light_layout = QtWidgets.QVBoxLayout(light_list_wdg)
+        self.light_layout.setContentsMargins(2, 2, 2, 2)
+        self.light_layout.setSpacing(3)
+        self.light_layout.setAlignment(QtCore.Qt.AlignTop)
+        
+        scroll_area = QtWidgets.QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setWidget(light_list_wdg)
 
         main_layout = QtWidgets.QVBoxLayout(self)
         main_layout.setContentsMargins(2, 2, 2, 2)
         main_layout.addLayout(header_layout)
-        main_layout.addStretch()
+        
+        main_layout.addWidget(scroll_area)
+        
         main_layout.addLayout(button_layout)
 
     def create_connections(self):
         self.refreshButton.clicked.connect(self.refresh_lights)
+        
+    
+    def get_lights_in_scene(self): 
+        """ Use Maya command to get list of lights in the scene """
+        return cmds.ls(type='light')
+        
 
     def refresh_lights(self):
-        print("TODO: refresh_lights()")
+        """ Redo list of all lists in dialog """
+        self.clear_lights()
+        
+        lights = self.get_lights_in_scene()
+        print('Lights: {}'.format(lights))
+        if lights:        
+            for light in lights:
+                light_item = LightItem(light)
+                self.light_items.append(light_item)
+                
+                self.light_layout.addWidget(light_item)
 
     def clear_lights(self):
-        print("TODO: clear_lights()")
+        """ Clear list of lists, should be called before refresh to purge list of lights """
+        self.light_items = []
+        
+        for i in range(self.light_layout.count()):
+            item = self.light_layout.itemAt(i) # get item on index
+            if item:
+                wdg = item.widget() # Returns the widget managed by this item
+                if wdg:
+                    wdg.deleteLater()
 
+    # called when Dialog shown (even for the first time)
     def showEvent(self, event):
         self.refresh_lights()
 
